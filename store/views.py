22c6888eff348afec0c65434ea9932c6df20d3a1
gaurav3210 <gs070599@gmail.com>
2019-07-11 15:39:26 +0530

    static images added

--- store/views.py
+++ store/views.py
@@ -13,11 +13,10 @@ def store(request):
 def store(request):
     count = Book.objects.all().count()
     context = {
-        'count':count,
-        'page': 'welcome to the bookkeeper bookstore'
+        'count': count,
     }
     request.session['location'] = "unknown"
     if request.user.is_authenticated:
         request.session['location'] = "Earth"
-    return render(request, 'store.html', context)
+    return render(request, 'base.html', context)
 
