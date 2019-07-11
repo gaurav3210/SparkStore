fe08afdae7025bd51f0db2379c2597e37646e557
gaurav3210 <gs070599@gmail.com>
2019-07-11 16:46:47 +0530

    Added css and Bootstrap modules

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Book
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
 '''
 def store(request):
     count = Book.objects.all().count()
